7162bbe63053dbb80eef57bde7e6b8e1879bd14c
Lucas Fauroux <lucas.fauroux@hec.edu>
2018-10-23 19:37:28 +0200

    Avec modif Rémi <3

--- src/pages/shops.tsx
+++ src/pages/shops.tsx
@@ -17,43 +17,46 @@ export class Shops extends React.Component {
     }
 }
 
-import GoogleMapReact from 'google-map-react'
-import CONFIG from '../../../config.json'
-import { markers, mapConfig } from '../../helper/utils'
+import React from 'react'
 
-import './style.styl'
+let __loaded: boolean = false
 
-const CustomMarker = ({ text }) => (
-    <div className="custom-marker">
-        <p>{text}</p>
-    </div>
-)
+export default class GoogleMap extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            mapIsReady: __loaded
+        }
+    }
 
-class GoogleMapReactComponent extends PureComponent {
+    componentDidMount() {
+        if (__loaded) return
+        const ApiKey = 'AIzaSyDa9smvZCA0L8-KlZ7Z2ioCL1ipRP6XZtI'
+        const script = document.createElement('script')
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${ApiKey}`
+        script.async = true
+        script.defer = true
+        script.addEventListener('load', () => {
+            __loaded = true
+            this.setState({ mapIsReady: true })
+        })
+
+        document.body.appendChild(script)
+    }
+
+    componentDidUpdate() {
+        if (this.state.mapIsReady) {
+            // Display the map
+            this.map = new window.google.maps.Map(document.getElementById('map'), {
+                center: { lat: -34.397, lng: 150.644 },
+                zoom: 12,
+                mapTypeId: 'roadmap'
+            })
+            // You also can add markers on the map below
+        }
+    }
+
     render() {
-        const GoogleMapsMarkers = markers.map(marker => (
-            <CustomMarker
-                key={`marker_${marker.name}`}
-                lat={marker.latlng[0]}
-                lng={marker.latlng[1]}
-                text={marker.name}
-            />
-        ))
-
-        return (
-            <GoogleMapReact
-                defaultCenter={mapConfig.center}
-                defaultZoom={mapConfig.zoom}
-                layerTypes={['TrafficLayer', 'TransitLayer']}
-                bootstrapURLKeys={{
-                    key: CONFIG.GOOGLE_MAPS_API_KEY,
-                    language: 'de'
-                }}
-            >
-                {GoogleMapsMarkers}
-            </GoogleMapReact>
-        )
+        return <div id="map" />
     }
 }
-
-export default GoogleMapReactComponent
