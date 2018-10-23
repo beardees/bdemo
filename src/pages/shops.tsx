d0731ca548e362e08528e1a98f819affee5cd4da
Lucas Fauroux <lucas.fauroux@hec.edu>
2018-10-23 19:21:35 +0200

    Adding Map2

--- src/pages/shops.tsx
+++ src/pages/shops.tsx
@@ -8,7 +8,7 @@ export class Shops extends React.Component {
         const addresses = Array.from(store.addresses.values())
         return (
             <div>
-                <h1>Page2</h1>
+                <h1>My Barber Shops</h1>
                 {addresses.map(shop => {
                     return <div className="beard">{JSON.stringify(shop)}</div>
                 })}
@@ -16,3 +16,44 @@ export class Shops extends React.Component {
         )
     }
 }
+
+import GoogleMapReact from 'google-map-react'
+import CONFIG from '../../../config.json'
+import { markers, mapConfig } from '../../helper/utils'
+
+import './style.styl'
+
+const CustomMarker = ({ text }) => (
+    <div className="custom-marker">
+        <p>{text}</p>
+    </div>
+)
+
+class GoogleMapReactComponent extends PureComponent {
+    render() {
+        const GoogleMapsMarkers = markers.map(marker => (
+            <CustomMarker
+                key={`marker_${marker.name}`}
+                lat={marker.latlng[0]}
+                lng={marker.latlng[1]}
+                text={marker.name}
+            />
+        ))
+
+        return (
+            <GoogleMapReact
+                defaultCenter={mapConfig.center}
+                defaultZoom={mapConfig.zoom}
+                layerTypes={['TrafficLayer', 'TransitLayer']}
+                bootstrapURLKeys={{
+                    key: CONFIG.GOOGLE_MAPS_API_KEY,
+                    language: 'de'
+                }}
+            >
+                {GoogleMapsMarkers}
+            </GoogleMapReact>
+        )
+    }
+}
+
+export default GoogleMapReactComponent
