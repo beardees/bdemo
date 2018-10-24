2cab5daf2e991383a3dc3ec2cc258a708e0c9174
Lucas Fauroux <lucas.fauroux@hec.edu>
2018-10-24 23:56:30 +0200

    New version Wednesday night

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -7,11 +7,30 @@ import { BeardCard } from './beardCard'
 export class Feed extends React.Component {
     render() {
         const beards = Array.from(store.beards.values())
+        const addresses = Array.from(store.addresses.values())
         return (
             <div>
                 <h1>Page2</h1>
                 {beards.map(beard => {
                     return <BeardCard beard={beard} />
+                })}
+                {addresses.map(a => {
+                    return (
+                        <div>
+                            [lat:
+                            {a.pos[0]}
+                            ,lon:
+                            {a.pos[1]}]
+                            {/* <img
+                                className="static-gmap"
+                                src={`//maps.googleapis.com/maps/api/staticmap?center=${
+                                    a.pos[0]
+                                },${
+                                    a.pos[1]
+                                }&zoom=13&size=600x300&maptype=roadmap&key=AIzaSyBNFwJ5SQRHwpnnhxUjFfSGm9Q8wl2-XuI`}
+                            /> */}
+                        </div>
+                    )
                 })}
             </div>
         )
